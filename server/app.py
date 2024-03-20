8be6fffb494705a8c94cb66bb4d358c880498beb
Genevieve Strittmatter <eastrittmatter@gmail.com>
2024-03-20 11:46:32 -0400

    added some constraints and started the flask routing for users

--- server/app.py
+++ server/app.py
@@ -3,15 +3,58 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import make_response, jsonify, request
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
 # Add your model imports
+from models import User, Prompt, Entry, Story
 
+class Users(Resource):
 
-# Views go here!
+    def get(self):
+        users = [user.to_dict() for user in User.query.all()]
+        return make_response(jsonify(users), 200)
+    
+    def post(self):
+        data = request.get_json()
+
+        try:
+            new_user = User(
+                name = data['name'],
+            )
+        except:
+            error = {"errors": ["validation errors"]}
+            return make_response(jsonify(error), 400)
+        
+        db.session.add(new_user)
+        db.session.commit()
+
+        return make_response(new_user.to_dict(), 201)
+    
+api.add_resource(Users, '/users')
+
+class UserByID(Resource):
+        
+    def get(self, id):
+        user = User.query.filter_by(id=id).first().to_dict()
+        if user:
+            return make_response(jsonify(user), 200)
+        error = {"error": "User not found"}
+        return make_response(jsonify(error), 404)
+
+    def patch(self, id):
+        user = User.query.filter_by(id=id).first()
+        for attr in request.json:
+            setattr(user, attr, request.json[attr])
+
+        db.session.add(user)
+        db.session.commit()
+
+        return make_response(user.to_dict(), 202)
+
+api.add_resource(UserByID, '/users/<int:id>')
 
 @app.route('/')
 def index():
--- server/models.py
+++ server/models.py
@@ -4,11 +4,16 @@ from sqlalchemy.orm import validates
 
 from config import db, metadata
 
+#the current setup removes all prompts and entries associated with a user if they delete their acct
+#might make more sense to just mark it as having a deleted acct?
+#might have overdone it on nullable=False foreign keys for similar reasons
+#maybe give User a "deleted" field that will allow their prompts/entries to persist
+
 class User(db.Model, SerializerMixin):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False, unique=True)
 
     prompts = db.relationship('Prompt', back_populates='user', cascade='all, delete-orphan')
     entries = db.relationship('Entry', back_populates='user', cascade='all, delete-orphan')
@@ -22,20 +27,33 @@ class Prompt(db.Model, SerializerMixin):
     __tablename__ = "prompts"
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
-    content = db.Column(db.String)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    title = db.Column(db.String, nullable=False)
+    content = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     user = db.relationship('User', back_populates='prompts')
+    stories = db.relationship('Story', back_populates='prompt', cascade='all, delete-orphan')
 
     serialize_rules = ('-user.prompts',)
 
     def __repr__(self):
         return f'<Prompt {self.id}, User id {self.user_id}, title {self.title}, content {self.content}>'
+    
+    @validates('title')
+    def validate_title(self, key, title):
+        if len(title) <= 100:
+            return title
+        raise ValueError('Prompt title must be 100 characters at most')
+    
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) <= 3000:
+            return content
+        raise ValueError('Prompt content must be 3000 characters at most')
 
 # should be able to use entry id to sort them for ui
-stories_entries = db.Table(
-    'stories_entries',
+story_entries = db.Table(
+    'story_entries',
     metadata,
     db.Column('story_id', db.Integer, db.ForeignKey(
         'stories.id'), primary_key=True),
@@ -47,10 +65,11 @@ class Entry(db.Model, SerializerMixin):
     __tablename__ = "entries"
 
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    content = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     user = db.relationship('User', back_populates='entries')
+    stories = db.relationship('Story', secondary=story_entries, back_populates='entries')
 
     serialize_rules = ('-user.entries',)
 
@@ -61,7 +80,10 @@ class Story(db.Model, SerializerMixin):
     __tablename__ = "stories"
 
     id = db.Column(db.Integer, primary_key=True)
-    prompt_id = db.Column(db.Integer, db.ForeignKey('prompts.id'))
+    prompt_id = db.Column(db.Integer, db.ForeignKey('prompts.id'), nullable=False)
+
+    prompt = db.relationship('Prompt', back_populates='stories')
+    entries = db.relationship('Entry', secondary=story_entries, back_populates='stories')
 
     def __repr__(self):
         return f'<Story {self.id}, prompt ID {self.prompt_id}>'
